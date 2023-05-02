eed007a1b2bbee1a91991d870f730cf2783a6659
Rodrigo Lazo <rodrigolazo.404@gmail.com>
2023-05-02 15:55:24 -0400

    Subiendo proyecto

---
+++ spring-webflux-api/src/test/java/cl/sterbe/app/SpringWebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sterbe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
